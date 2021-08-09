548f155b57bfda6047b64c3dad9a6f9f01afdd39
peterkuria <peter.kuria.cloud@gmail.com>
2021-08-10 00:42:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CargoShip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Peter!");
+            Console.ReadLine(); // keeps the console on
+
+        }
+    }
+}
